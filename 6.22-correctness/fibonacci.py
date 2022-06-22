71708e7da7c8d30c28e25c6ea6528b7aa217fa46
Christopher Siu <christopher@siu.sh>
2022-06-22 11:06:46 -0700

    Added recursive example with coverage.

--- 6.22-correctness/fibonacci.py
+++ 6.22-correctness/fibonacci.py
@@ -1,21 +1,40 @@
 import sys
 
 
+# To compute the nth Fibonacci number iteratively:
+# def fibonacci(n):
+#     current = previous = None
+#
+#     for i in range(n + 1):
+#         if i == 0:
+#             current = 0
+#         elif i == 1:
+#             previous = current
+#             current = 1
+#         else:
+#             temp = previous
+#             previous = current
+#             current = current + temp
+#
+#         print("previous = %s, current = %s" % (repr(previous), repr(current)))
+#
+#     return current
+
+
+# To compute the nth Fibonacci number recursively:
 def fibonacci(n):
-    current = previous = None
-
-    for i in range(n + 1):
-        if i == 0:
-            current = 0
-        elif i == 1:
-            previous = current
-            current = 1
-        else:
-            temp = previous
-            previous = current
-            current = current + temp
-
-    return current
+    # The base cases should address the smallest possible problems -- since
+    #  the Fibonacci numbers are recursively defined, that definition tells
+    #  us what the smallest possible problems and their solutions are.
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    # The recursive cases should reduce larger problems into smaller problems
+    #  -- since the Fibonacci numbers are recursively defined, that definition
+    #  tells us how smaller problems fit into larger problems.
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 if __name__ == "__main__":
--- 6.22-correctness/fibonacci_tests.py
+++ 6.22-correctness/fibonacci_tests.py
@@ -3,8 +3,18 @@ import fibonacci as fib
 
 
 class TestFibonacci(unittest.TestCase):
+    # NOTE: This test alone is not enough to conclude correctness: a function
+    #       that simply *always* returned 0 would pass this test...
     def test01(self):
         self.assertEqual(fib.fibonacci(0), 0)
+
+    def test02(self):
+        self.assertEqual(fib.fibonacci(1), 1)
+
+    # NOTE: The above tests do not test the recursive case, and thus do not
+    #       have 100% coverage.
+    def test03(self):
+        self.assertEqual(fib.fibonacci(3), 2)
 
 
 if __name__ == "__main__":
