22e72f59b59aeccc6271d8ee5212c475a51b86a9
Christopher Siu <christopher@siu.sh>
2022-07-06 07:32:52 -0700

    Added base array list example.

---
+++ 7.06-array-lists/array_list.py
@@ -0,0 +1,10 @@
+class List:
+    """ An ordered collection of elements """
+
+    def __init__(self):
+        # The length of the backing array:
+        self.capacity = 4
+        # The backing array:
+        self.array = [None] * self.capacity
+        # The number of elements in this list:
+        self.size = 0
