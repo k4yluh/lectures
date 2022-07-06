4dd24db109d8012c49d214629cb3c47215cff0a8
Kayla Tran <52841240+k4yluh@users.noreply.github.com>
2022-07-06 13:45:39 -0700

    Update linked_list.py

--- 6.29-linked-lists/linked_list.py
+++ 6.29-linked-lists/linked_list.py
@@ -1,5 +1,13 @@
+# Implements a linked list.
+# CSC 202, Lab 2
+# Given code, Summer '19
+
+
 class List:
-    """ An ordered collection of elements """
+    """
+    An ordered collection of elements
+    NOTE: Do not alter this class.
+    """
 
     def __init__(self):
         # The head of the backing linked list:
@@ -7,9 +15,20 @@ class List:
         # The number of elements in this list:
         self.size = 0
 
+    def __eq__(self, other):
+        return (type(other) == List
+                and self.head == other.head
+                and self.size == other.size)
+
+    def __repr__(self):
+        return "List(%r, %d)" % (self.head, self.size)
+
 
 class Node:
-    """ A single node in a linked list """
+    """
+    A single node in a linked list
+    NOTE: Do not alter this class.
+    """
 
     def __init__(self, value, next):
         # The value contained in this node:
@@ -17,51 +36,124 @@ class Node:
         # The next node in the linked list:
         self.next = next
 
+    def __eq__(self, other):
+        return (type(other) == Node
+                and self.value == other.value
+                and self.next == other.next)
+
+    def __repr__(self):
+        return "Node(%r, %r)" % (self.value, self.next)
+
+
+def size(lst):
+    """
+    Count the number of elements in a list.
+    TODO: Implement this function. It must have O(1) complexity.
+    :param lst: A List
+    :return: The number of elements in the list
+    """
+    count = 0
+    for e in range(lst.size):
+        count += 1
+    return count
+
 
 def get(lst, idx):
-    # Set a current node reference to the head.
-    # Set a counter to 0.
-    # For counter from 0 to idx, do:
-    #     Set the current node to the current node's next.
-    # Return the current node's value.
-    pass
+    """
+    Get the element at an index.
+    TODO: Implement this function. It must have O(n) complexity.
+    :param lst: A List
+    :param idx: An index at which to get an element
+    :return: The element in the list at the index
+    :raise IndexError: If the index is out-of-bounds
+    """
+    if idx < 0 or idx > lst.size:
+        raise IndexError
+
+    cur = lst.head
+    for i in range(idx):
+        cur = cur.next
+    return cur.value
 
 
 def set(lst, idx, value):
-    # Set a current node reference to the head.
-    # Set a counter to 0.
-    # For counter from 0 to idx, do:
-    #     Set the current node to the current node's next.
-    # Set the current node's value to the given value. 
-    pass
+    """
+    Set the element at an index.
+    TODO: Implement this function. It must have O(n) complexity.
+    :param lst: A List
+    :param idx: An index at which to set an element
+    :param value: A value to which to set an element
+    :raise IndexError: If the index is out-of-bounds
+    """
+    if idx < 0 or idx > lst.size - 1:
+        raise IndexError
+
+    cur_n = lst.head
+    for i in range(idx):
+        cur_n = cur_n.next
+    cur_n.value = value
 
 
 def add(lst, idx, value):
-    # If adding to the head:
-    #     Make a new node
-    #     Set the new node's next to the head -- this still works if the list
-    #      was empty; it just makes the new node's next None, which is correct
-    #     Set the head to that new node
-    # Else if adding to the end of a non-empty list:
-    #     Make a new node with no "next"
-    #     Find the node *before* the desired index
-    #     Set the previous node's next to the new node
-    # Else if adding to the middle of a non-empty list:
-    #     Make a new node
-    #     Find the node *before* the desired index
-    #     Set the new node's next to the previous node's next -- this *must*
-    #      be done before changing the previous node's next, which is the only
-    #      way to access the rest of the nodes
-    #     Set the previous node's next to the new node
-    pass
+    """
+    Add an element at an index.
+    TODO: Implement this function. It must have O(n) complexity.
+    :param lst: A List
+    :param idx: An index at which to add an element
+    :param value: A value to add as an element
+    :raise IndexError: If the index is out-of-bounds
+    """
+    if idx < 0 or idx > lst.size:
+        raise IndexError
+
+    if idx == 0:
+        new = Node(value, lst.head)
+        lst.head = new
+    elif idx == lst.size and lst.size > 0:
+        new = Node(value, None)
+        prev = lst.head
+        for i in range(idx - 1):
+            prev = prev.next
+        prev.next = new
+    elif 0 < idx < lst.size:
+        new = Node(value, None)
+        prev = lst.head
+        for i in range(idx - 1):
+            prev = prev.next
+        new.next = prev.next
+        prev.next = new
+    lst.size += 1
 
 
 def remove(lst, idx):
-    # ...
-    # Else if removing from the middle of a non-empty list:
-    #     Find the node *before* the desired index
-    #     Get the previous node's next's next -- this is just the node *after*
-    #      the desired index.
-    #     Set the previous node's next to that successor node, thereby
-    #      "unlinking" the removed node from the chain of nodes.
-    pass
+    """
+    Remove the element at an index.
+    TODO: Implement this function. It must have O(n) complexity.
+    :param lst: A List
+    :param idx: An index at which to remove an element
+    :raise IndexError: If the index is out-of-bounds
+    :return: The removed element
+    """
+    if idx < 0 or idx > lst.size - 1:
+        raise IndexError
+    removed = 0
+    if idx == 0:
+        removed = lst.head
+        lst.head = lst.head.next
+    elif 0 < idx < lst.size - 1:
+        prev = lst.head
+        aft = lst.head
+        for i in range(idx - 1):
+            prev = prev.next
+        removed = prev.next.next
+        for i in range(idx + 1):
+            aft = aft.next
+        prev.next = aft
+    elif idx == lst.size and lst.size > 0:
+        cur = lst.head
+        for i in range(idx):
+            cur = cur.next
+        removed = cur.value
+        cur.value = None
+    lst.size -= 1
+    return removed
