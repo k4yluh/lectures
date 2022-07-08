91299509d61b6df51f94ded516c0baca425758b8
Christopher Siu <christopher@siu.sh>
2022-07-08 10:57:25 -0700

    Added stack pseudocode.

--- 7.08-stacks/array_stack.py
+++ 7.08-stacks/array_stack.py
@@ -8,3 +8,26 @@ class Stack:
         self.array = [None] * self.capacity
         # The number of elements in this stack:
         self.size = 0
+
+
+def push(stack, value):
+    # If the size is equal to the capacity:
+    #     Double the capacity.
+    #     Create a new array with that new capacity.
+    #     For i from 0 to the size of the list:
+    #         Set new_array[i] to array[i].
+    #     Set the list's array to that new array.
+    # Set array[size] to the given value.
+    # Increment the size.
+    pass
+
+
+def pop(stack):
+    # Return array[size - 1].
+    # Decrement the size.
+    pass
+
+
+def peek(stack);
+    # Return array[size - 1].
+    pass
--- 7.08-stacks/linked_stack.py
+++ 7.08-stacks/linked_stack.py
@@ -16,3 +16,29 @@ class Node:
         self.value = value
         # The next node in the linked list:
         self.next = next
+
+
+def push(stack, value):
+    # If the stack is empty:
+    #     Create a new node with no next.
+    #     Set the new node's value to the given value.
+    #     Set the head to that new node.
+    # Else:
+    #     Create a new node whose next is the old head.
+    #     Set the new node's value to the given value.
+    #     Set the head to that new node.
+    # Increment the size.
+    pass
+
+
+def pop(stack):
+    # Save the head's value in a temporary variable.
+    # Set the head to the head's next.
+    # Return the temporary variable.
+    # Decrement the size.
+    pass
+
+
+def peek(stack):
+    # Return the head's value.
+    pass
