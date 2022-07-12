753fcccf2f79ef7d02129a8830afacd34e8a466c
Christopher Siu <christopher@siu.sh>
2022-07-12 11:32:49 -0700

    Added base array queue example.

---
+++ 7.13-circular-queues/array_queue.py
@@ -0,0 +1,10 @@
+class Queue:
+    """ A FIFO collection of elements """
+
+    def __init__(self):
+        # The length of the backing array:
+        self.capacity = 4
+        # The backing array:
+        self.array = [None] * self.capacity
+        # The number of elements in this queue:
+        self.size = 0
