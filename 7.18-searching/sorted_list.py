1f56a1ba67bf0f13adf59700eac689197831d31b
Christopher Siu <christopher@siu.sh>
2022-07-18 10:55:09 -0700

    Added binary search pseudocode.

--- 7.18-searching/sorted_list.py
+++ 7.18-searching/sorted_list.py
@@ -1,6 +1,13 @@
 class SortedList:
     """ A sorted collection of elements """
 
+    # NOTE: The list must be sorted in order to perform a binary search, else
+    #       comparing the middle element eliminates only the middle element.
+    # NOTE: The list must be backed by an array, so that it can support fast
+    #       random access. With a linked list, comparing the midpoint instead
+    #       of the first element saves no time, because simply getting to the
+    #       midpoint requires traversing the entire first half.
+
     def __init__(self):
         # The length of the backing array:
         self.capacity = 4
@@ -8,3 +15,21 @@ class SortedList:
         self.array = [None] * self.capacity
         # The number of elements in this list:
         self.size = 0
+
+
+def find(lst, value):
+    # NOTE: Every comparison eliminates about half the remaining elements;
+    #       in the worst case scenario, the number of comparisons is equal to
+    #       the number of times the list can be halved until it is empty: this
+    #       is the number of times n can be divided by 2: approx. log_2 n.
+
+    # Set low to 0 and high to size - 1.
+    # While low <= high:
+    #     Set mid to (low + high) // 2.
+    #     If array[mid] < value:
+    #         Set low to mid + 1.
+    #     Else if array[mid] > value:
+    #         Set high to mid - 1. 
+    #     Else:
+    #         Return mid
+    # Return None
