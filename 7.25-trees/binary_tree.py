68a01867fa3a56c6e1720334063e21a7da9c9ae1
Christopher Siu <christopher@siu.sh>
2022-07-25 11:02:48 -0700

    Added binary tree and preorder traversal pseudocode.

--- 7.25-trees/binary_tree.py
+++ 7.25-trees/binary_tree.py
@@ -27,10 +27,31 @@ class Node:
 
 
 def combine(value, left, right):
+    # Make a new tree whose root node contains the given value.
+    # Set the root's left child to the root of the given left tree.
+    # Set the root's right child to the root of the given right tree.
+    # Set the new tree's size to size of left + size of right + 1.
+    # Return the new tree.
     pass
 
 
 def preorder(tree):
+    # NOTE: Binary trees are recursively defined structures:
+    #         * Base case: The smallest possible tree has 0 or 1 nodes.
+    #         * Recursive case: All other trees can be constructed by
+    #           combining two smaller trees as the children of a new root.
+    #       ...binary trees can be traversed by recursive functions.
+
+    # If the root of the given tree does not exist, then:
+    #     Return (whatever you were asked to return).
+    # Else if the root has no children, then:
+    #     Mark the root as "explored" (do whatever you came here to do).
+    #     Return.
+    # Else, do:
+    #     Mark the root as "explored".
+    #     Recurse on the left subtree.
+    #     Recurse on the right subtree.
+    #     Return.
     pass
 
 
