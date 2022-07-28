93f3157217872dac79bc90c70055aff4863c7b90
Christopher Siu <christopher@siu.sh>
2022-07-28 08:25:25 -0700

    Added base BST example.

---
+++ 7.29-bsts/bst.py
@@ -0,0 +1,36 @@
+class BinarySearchTree:
+    """ A binary search tree """
+
+    def __init__(self):
+        # The root of this tree:
+        self.root = None
+        # The number of nodes in this tree:
+        self.size = 0
+
+
+class Node:
+    """ A single node in a binary search tree """
+
+    def __init__(self, key, left, right):
+        # The key contained in this node:
+        self.key = key
+        # The left child of this node:
+        self.left = left
+        # The right child of this node:
+        self.right = right
+
+
+def find(bst, key):
+    pass
+
+
+def insert(bst, key):
+    pass
+
+
+def remove(bst, key):
+    pass
+
+
+def inorder(bst):
+    pass
