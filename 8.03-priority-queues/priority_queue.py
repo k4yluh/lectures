c6471cd82958e29624e17e5225114ab1baf66e1c
Christopher Siu <christopher@siu.sh>
2022-08-03 10:59:42 -0700

    Added outline of priority queue concepts.

--- 8.03-priority-queues/priority_queue.py
+++ 8.03-priority-queues/priority_queue.py
@@ -5,9 +5,15 @@ class PriorityQueue:
         # The length of the backing array:
         self.capacity = 4
         # The backing array, placeholder at index 0:
+        # NOTE: To make the indexing math work out nicely, there can't be any
+        #       "nodes" at index 0.
         self.array = [None] * (self.capacity + 1)
         # The number of elements in this queue:
         self.size = 0
+
+
+# NOTE: The array represents a complete binary tree in which every node is
+#       larger than its children: a "binary heap".
 
 
 def enqueue(pqueue, value):
@@ -19,4 +25,5 @@ def dequeue(pqueue):
 
 
 def peek(pqueue):
+    # Return the root (whose index is always 1).
     pass
