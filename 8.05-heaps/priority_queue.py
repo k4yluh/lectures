13b58a91e7053a065912d14ffa3d34acc39fbb83
Christopher Siu <christopher@siu.sh>
2022-08-04 08:02:18 -0700

    Added base binary heap example.

---
+++ 8.05-heaps/priority_queue.py
@@ -0,0 +1,23 @@
+class PriorityQueue:
+    """ A prioritized collection of elements """
+
+    def __init__(self):
+        # The length of the backing array:
+        self.capacity = 4
+        # The backing array, placeholder at index 0:
+        self.array = [None] * (self.capacity + 1)
+        # The number of elements in this queue:
+        self.size = 0
+
+
+def enqueue(pqueue, value):
+    pass
+
+
+def dequeue(pqueue):
+    pass
+
+
+def peek(pqueue):
+    # Return the root (whose index is always 1).
+    pass
