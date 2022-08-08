8cf7fd36a194cf82a36f2e0f7ad18305304b4e90
Christopher Siu <christopher@siu.sh>
2022-08-08 07:24:34 -0700

    Added base dictionary example.

---
+++ 8.08-dictionaries/dictionary.py
@@ -0,0 +1,22 @@
+class Dictionary:
+    """ A collection of key-value pairs """
+
+    def __init__(self):
+        # The length of the backing array:
+        self.capacity = 5
+        # The backing array:
+        self.array = [None] * self.capacity
+        # The number of pairs in this dictionary:
+        self.size = 0
+
+
+def get(dct, key):
+    pass
+
+
+def insert(dct, key, value):
+    pass
+
+
+def remove(dct, key):
+    pass
