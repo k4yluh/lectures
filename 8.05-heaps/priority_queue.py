bcf01c36c23d54d6296fd33ce76a0d8815bf683f
Kayla Tran <52841240+k4yluh@users.noreply.github.com>
2022-08-10 11:53:52 -0700

    Update priority_queue.py

--- 8.05-heaps/priority_queue.py
+++ 8.05-heaps/priority_queue.py
@@ -1,60 +1,110 @@
+# Implements a priority queue.
+# CSC 202, Lab 8
+# Given code, Summer '19
+
+
+# import pwd
+
+
 class PriorityQueue:
-    """ A prioritized collection of elements """
+    """
+    A prioritized collection of elements
+    NOTE: Do not alter this class.
+    """
 
     def __init__(self):
         # The length of the backing array:
         self.capacity = 4
         # The backing array, placeholder at index 0:
-        # NOTE: Complete binary trees can be stored in arrays, where:
-        #         * The "root" is always index 1.
-        #         * The "parent" of index i is index i // 2.
-        #         * The "children" of index i are indices 2i and 2i + 1.
         self.array = [None] * (self.capacity + 1)
         # The number of elements in this queue:
         self.size = 0
 
+    def __eq__(self, other):
+        if type(other) != PriorityQueue or self.size != other.size:
+            return False
+
+        for idx in range(1, self.size + 1):
+            if self.array[idx] != other.array[idx]:
+                return False
+
+        return True
+
+    def __repr__(self):
+        return "PriorityQueue(%d, %r, %d)"\
+               % (self.capacity, self.array, self.size)
+
+
+def size(pqueue):
+    """
+    Calculate the size of a priority queue.
+    TODO: Implement this function. It must have O(1) complexity.
+
+    :param pqueue: A PriorityQueue
+    :return: The number of elements in the priority queue
+    """
+    return pqueue.size
+
 
 def enqueue(pqueue, value):
-    # NOTE: Elements must always be added at the next leaf. Since the tree
-    #       is complete, the next leaf is always index size + 1.
-    # NOTE: The maximum number of swaps is the height of the tree -- this
-    #       loop will never run more than O(log n) times. However, *most* of
-    #       the nodes in this tree are leaves. Most elements require only
-    #       one or two swaps, because they are already close to where they
-    #       need to be.
+    """
+    Enqueue an element with some priority to a priority queue.
+    TODO: Implement this function. It must have O(1) complexity.
 
-    # If the array is at capacity:
-    #     Double the capacity.
-    #     Create a new array with new capacity + 1.
-    #     Copy the elements of the old array into the new array.
-    # Set the next leaf (index size + 1) to the given value.
-    # While the value is bigger than its parent (index current // 2), then:
-    #     Swap the value with its parent.
-    # Increment the size.
-    pass
+    :param pqueue: A PriorityQueue
+    :param value: A comparable value to be enqueued
+    """
+    if pqueue.size == pqueue.capacity:
+        pqueue.capacity *= 2
+        temp = pqueue.array
+        pqueue.array = [None] * (pqueue.capacity+1)
+        for i in range(pqueue.size):
+            pqueue.array[i] = temp[i]
+    pqueue.array[pqueue.size + 1] = value
+    while value != pqueue.array[1] and value > pqueue.array[(pqueue.size + 1)//2]:
+        temp = pqueue.array[(pqueue.size + 1)//2]
+        pqueue.array[(pqueue.size + 1)//2] = value
+        value = temp
+    pqueue.size += 1
 
 
 def dequeue(pqueue):
-    # NOTE: Nodes must always be removed from the last leaf. Since the tree
-    #       is complete, the last leaf is always index size.
-    # NOTE: The maximum number of swaps is the height of the tree -- this
-    #       loop will never run more than O(log n) times. However, *most* of
-    #       the nodes in this tree are leaves. Most elements require almost
-    #       all of the swaps, because they are moved into the root, which is
-    #       far from where they need to be.
+    """
+    Dequeue the element of highest priority from a priority queue.
+    TODO: Implement this function. It must have O(log n) complexity.
 
-    # Set the root (index 1) to the last leaf (index size).
-    # While the current has children larger than itself, do:
-    #     If the left child (index 2i) is bigger than the right child
-    #      (index 2i + 1) and bigger than the current, then:
-    #         Swap the current with its left child.
-    #     Else if the right child is bigger than the current, then:
-    #         Swap the current with its right child.
-    # Decrement the size.
-    # Return the old root.
-    pass
+    :param pqueue: A PriorityQueue
+    :return: The dequeued element
+    :raise ValueError: If the priority queue is empty
+    """
+    if pqueue.size == 0:
+        raise ValueError
+    pqueue.array[1] = pqueue.array[pqueue.size]
+    cur = 1
+    while pqueue.array[cur] < pqueue.array[(2*cur)] and pqueue.array[cur] < pqueue.array[(2*cur+1)]:
+        if pqueue.array[2*cur] > pqueue.array[2*cur+1] and pqueue.array[2*cur] > pqueue.array[cur]:
+            temp = pqueue.array[2*cur]
+            pqueue.array[2*cur] = pqueue.array[cur]
+            pqueue.array[cur] = temp
+        elif pqueue.array[(2*cur+1)] > pqueue.array[cur]:
+            temp = pqueue.array[(2*cur+1)]
+            pqueue.array[(2*cur+1)] = pqueue.array[cur]
+            pqueue.array[cur] = temp
+    pqueue.size -= 1
+    return temp 
 
 
 def peek(pqueue):
-    # Return the root (index 1).
-    pass
+    """
+    Peek at the element of highest priority in a queue.
+    TODO: Implement this function. It must have O(1) complexity.
+
+    :param pqueue: A PriorityQueue
+    :return: The peeked element
+    :raise ValueError: If the priority queue is empty
+    """
+    if pqueue.size == 0:
+        raise ValueError
+    else:
+        return pqueue.array[1]
+        
